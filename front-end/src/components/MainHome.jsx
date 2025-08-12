41585a0df87ff5ea0be6c86cf096cb421033e29f
DavideCriscuolo <davide.alcatel@gmail.com>
2025-08-12 11:14:09 +0200

    aggiunta pack SupaBase per la parte di Back-end

---
+++ front-end/src/components/MainHome.jsx
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
+); // collego supabase al sito
+
+export default function MainHome() {
+  return <></>;
+}
