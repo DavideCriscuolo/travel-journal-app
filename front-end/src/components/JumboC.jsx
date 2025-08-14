53da7a228cdbd0996dc73b0267be46088210c90e
DavideCriscuolo <davide.alcatel@gmail.com>
2025-08-14 18:15:35 +0200

    implementato filtro per la ricerca di un viaggio contente il valore immesso come tag e come spesa

--- front-end/src/components/JumboC.jsx
+++ front-end/src/components/JumboC.jsx
@@ -2,7 +2,7 @@ export default function JumboC() {
   return (
     <>
       <div className="p-5 mb-4 h-100 rounded-3">
-        <div className="container-fluid h-100 d-flex flex-column justify-content-end py-5">
+        <div className="container-fluid h-100 d-flex flex-column justify-content-start py-5">
           <h1 className="display-5 text-white fw-bold">Diario di Viaggio</h1>
           <p className="col-md-8 text-white fs-4">
             Questo è il mio diario di viaggio per le mie vacanze estive
--- front-end/src/components/MainHome.jsx
+++ front-end/src/components/MainHome.jsx
@@ -6,7 +6,9 @@ import "./../css/MainHome.css";
 import CardC from "./CardC";
 export default function MainHome() {
   const [juorneys, setJuorneys] = useState([]);
-
+  const [spesaT, setSpesaT] = useState(""); //variabile di stato per la ricerca per spesa
+  const [juorneySerached, setJuorneySerached] = useState([]);
+  const [tags, setTags] = useState([]);
   useEffect(() => {
     const fetchAll = async () => {
       const { data, error } = await supabase.from(`travel_journal`).select(`*`); //seleziona tutte le colonne della tabella travel_journal
@@ -21,6 +23,24 @@ export default function MainHome() {
     fetchAll();
   }, []);
 
+  const search = async (e) => {
+    console.log(tags);
+    console.log(spesaT);
+    e.preventDefault();
+
+    const { data, error } = await supabase
+      .from(`travel_journal`)
+      .select(`*`)
+      .or(`tags.ilike.%${tags}%,spesa.eq.${spesaT}`);
+
+    if (error) {
+      console.log(error);
+    } else {
+      setJuorneySerached(data);
+      console.log(tags);
+    }
+  };
+
   return (
     <>
       <main className="bg_main_home">
@@ -28,6 +48,57 @@ export default function MainHome() {
           <h1 className="text-center text-white">
             I miei viaggi <span className="emoji bounce">🛩️</span>{" "}
           </h1>
+          <div className="p-5">
+            <h4 className="text-center text-white p-3">
+              Cerca nei miei Viaggi
+            </h4>
+            <div className="d-flex justify-content-center">
+              <form
+                className="d-flex justify-content-between align-items-center gap-3"
+                onSubmit={search}
+                action=""
+              >
+                <input
+                  type="text"
+                  className="form-control"
+                  onChange={(e) => setTags(e.target.value)}
+                  value={tags}
+                  placeholder="Cerca per tags"
+                  required
+                />
+
+                <input
+                  type="number"
+                  className="form-control"
+                  onChange={(e) => setSpesaT(e.target.value.toString())}
+                  value={spesaT}
+                  placeholder="Cerca per spesa totale"
+                  required
+                />
+                <button className="btn btn-primary" type="submit">
+                  cerca
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 p-5  g-3">
+            {juorneySerached.map((item) => {
+              return (
+                <div>
+                  <div>
+                    <CardC
+                      key={item.id}
+                      id={item.id}
+                      img={item.img}
+                      luogo={item.luogo}
+                    ></CardC>
+                    <h4 className="py-2  text-white">{item.tags}</h4>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
           <div className="row row-cols-1 row-cols-sm--2 row-cols-md-3 row-cols-lg-5 p-5  g-3">
             {juorneys.map((item) => {
               return (
